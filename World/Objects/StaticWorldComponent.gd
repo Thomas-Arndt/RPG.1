46b5157c2ca86cb9b7937b08b38f6aa0efb2525f
Thomas Arndt <arndtt42@gmail.com>
2022-05-26 16:39:43 -0700

    Added StaticWorldComponent with dimension matching functionality

---
+++ World/Objects/StaticWorldComponent.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+onready var red_sprite = $RedSprite
+onready var green_sprite = $GreenSprite
+
+func _ready():
+	match_dimension()
+
+func _process(delta):
+	match_dimension()
+	
+func match_dimension():
+	if WorldStats.DIMENSION == true:
+		red_sprite.visible = true
+		green_sprite.visible = false
+	else:
+		red_sprite.visible = false
+		green_sprite.visible = true
