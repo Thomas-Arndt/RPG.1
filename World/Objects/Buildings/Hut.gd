2e09dc624cc1ac559fb8df6b1710b2b228717b24
Thomas Arndt <arndtt42@gmail.com>
2022-06-17 08:29:38 -0700

    Added dimension-matching for hut sprite

--- World/Objects/Buildings/Hut.gd
+++ World/Objects/Buildings/Hut.gd
@@ -1,9 +1,23 @@
 extends StaticBody2D
 
 onready var Footprint = $Footprint
+onready var red_sprite = $RedSprite
+onready var green_sprite = $GreenSprite
 
 func _ready():
 	Footprint.set_deferred("disabled", false)
+	match_dimension()
+
+func _process(delta):
+	match_dimension()
+	
+func match_dimension():
+	if WorldStats.DIMENSION == true:
+		red_sprite.visible = true
+		green_sprite.visible = false
+	else:
+		red_sprite.visible = false
+		green_sprite.visible = true
 
 func _on_AutomaticEntry_body_entered(body):
 	Footprint.set_deferred("disabled", true)
