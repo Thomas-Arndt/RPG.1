9d71f9be07f122187da6ae879ce5f6c205235eaf
Thomas Arndt <arndtt42@gmail.com>
2022-06-25 14:39:50 -0700

    Addition of framework for new Bat enemy scene

---
+++ Enemies/Bat.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+
+
+
+func _on_Stats_no_health():
+	pass # Replace with function body.
+
+
+func _on_HurtBox_invincible_end():
+	pass # Replace with function body.
+
+
+func _on_HurtBox_invincible_start():
+	pass # Replace with function body.
+
+
+func _on_HurtBox_area_entered(area):
+	pass # Replace with function body.
