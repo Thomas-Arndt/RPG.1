e04f1a17a6c6364dcd292500f9cbcb678c081e55
Thomas Arndt <arndtt42@gmail.com>
2022-06-26 09:51:07 -0700

    Completion of Bat state machine, wit ha couple of bug fixes inolving hit and hurt box collision layers

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,19 +1,97 @@
 extends KinematicBody2D
 
+export var ACCELERATION = 200
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var WANDER_BUFFER = 4
 
+enum {
+	IDLE,
+	WANDER,
+	CHASE,
+}
 
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+
+var state = IDLE
+
+onready var stats = $Stats
+onready var player_detection_zone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
+onready var hurt_box = $HurtBox
+onready var soft_collisions = $SoftCollision
+onready var wander_controller = $WanderController
+onready var blink_animation_player = $BlinkAnimationPlayer
+
+func _ready():
+	blink_animation_player.play("stop")
+	state = pick_random_state([IDLE, WANDER])
+	
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+			if wander_controller.get_time_left() == 0:
+				update_wander()
+		
+		WANDER:
+			seek_player()
+			
+			if wander_controller.get_time_left() == 0:
+				update_wander()
+				
+			accelerate_towards_point(wander_controller.target_position, delta)
+			
+			if global_position.distance_to(wander_controller.target_position) <= WANDER_BUFFER:
+				update_wander()
+		
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				accelerate_towards_point(player.global_position, delta)
+			else:
+				state = IDLE
+		
+	if soft_collisions.is_colliding():
+		velocity += soft_collisions.get_push_vector() * delta * 400
+	velocity = move_and_slide(velocity)
+			
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wander_controller.start_wander_timer(rand_range(1,3))
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, FRICTION * delta)
+	sprite.flip_h = velocity.x < 0
+
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
 
 func _on_Stats_no_health():
-	pass # Replace with function body.
+	queue_free()
 
 
 func _on_HurtBox_invincible_end():
-	pass # Replace with function body.
+	blink_animation_player.play("stop")
 
 
 func _on_HurtBox_invincible_start():
-	pass # Replace with function body.
+	blink_animation_player.play("start")
 
 
 func _on_HurtBox_area_entered(area):
-	pass # Replace with function body.
+	stats.change_health(-area.damage)
+	knockback = area.knockback_vector * 130
+	hurt_box.start_invincible(0.4)
