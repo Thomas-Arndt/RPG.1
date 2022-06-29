a0454d6381c68022bddb72862394ab71fea1e71f
Thomas Arndt <arndtt42@gmail.com>
2022-06-29 15:43:16 -0700

    Added tween to smooth health bar change animation

--- UI/HealthBar.gd
+++ UI/HealthBar.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const CHAR_READ_RATE = 0.05
+
 export var health = 10 setget set_health
 export var max_health = 10 setget set_max_health
 
@@ -10,11 +12,17 @@ func _ready():
 	self.set_health(PlayerStats.max_health)
 	PlayerStats.connect("health_changed", self, "set_health")
 	PlayerStats.connect("max_health_changed", self, "set_max_health")
+	healthFill.rect_size.x = 100
 
 func set_health(value):
-	health = clamp(value, 0, max_health)
 	var maxHealthPercent: float = 100 / max_health
 	healthFill.rect_size.x = health * maxHealthPercent
+	$Tween.remove_all()
+	var start_rect_x = health * maxHealthPercent
+	health = clamp(value, 0, max_health)
+	var end_rect_x = health * maxHealthPercent
+	$Tween.interpolate_property(healthFill, "rect_size:x", start_rect_x, end_rect_x, abs(start_rect_x - end_rect_x) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
 
 func set_max_health(value):
 	max_health = max(value, 1)
