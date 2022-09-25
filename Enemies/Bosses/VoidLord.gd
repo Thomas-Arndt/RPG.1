cfa9cb7a1fa66d88e5d98c155f5bf7f8421575b0
Thomas Arndt <arndtt42@gmail.com>
2022-09-25 14:04:27 -0700

    Added Stats scene and ability for player to do damage to Void Lords

--- Enemies/Bosses/VoidLord.gd
+++ Enemies/Bosses/VoidLord.gd
@@ -4,6 +4,7 @@ export (bool) var is_red = true
 
 onready var blink_anim_player = $BlinkAnimationPlayer
 onready var hurt_box = $HurtBox
+onready var stats = $Stats
 onready var red_sprite_full = $RedSpriteFull
 onready var red_sprite_half = $RedSpriteHalf
 onready var green_sprite_full = $GrenSpriteFull
@@ -18,15 +19,21 @@ func match_dimension(state):
 	red_sprite_half.visible = false
 	green_sprite_full.visible = false
 	green_sprite_half.visible = false
+	hurt_box.monitorable = false
+	hurt_box.monitoring = false
 	
 	if state == true and is_red:
 		red_sprite_full.visible = true
+		hurt_box.monitorable = true
+		hurt_box.monitoring = true
 	elif state == false and is_red:
 		red_sprite_half.visible = true
 	elif state == true and not is_red:
 		green_sprite_half.visible = true
 	elif state == false and not is_red:
 		green_sprite_full.visible = true
+		hurt_box.monitorable = true
+		hurt_box.monitoring = true
 
 func _on_HurtBox_invincible_start():
 	blink_anim_player.play("start")
@@ -37,4 +44,9 @@ func _on_HurtBox_invincible_end():
 
 
 func _on_HurtBox_area_entered(area):
+	stats.change_health(-area.damage)
 	hurt_box.start_invincible(0.6)
+
+
+func _on_Stats_no_health():
+	queue_free()
