448ac3ab95cfbbed62ea1f2c7b724289f9166295
Thomas Arndt <arndtt42@gmail.com>
2022-10-01 14:35:36 -0700

    Addition of DetectionWanderControler and in use with void lord to begin setup of void lord battle logic

--- Enemies/Bosses/VoidLord.gd
+++ Enemies/Bosses/VoidLord.gd
@@ -1,10 +1,17 @@
 extends KinematicBody2D
 
+export var ACCELERATION = 200
+export var MAX_SPEED = 100
+export var FRICTION = 200
+
 export (bool) var is_red = true
+
+var velocity: Vector2 = Vector2.ZERO
 
 onready var blink_anim_player = $BlinkAnimationPlayer
 onready var hurt_box = $HurtBox
 onready var stats = $Stats
+onready var wander_controller = $DetectionWanderController
 onready var red_sprite_full = $RedSpriteFull
 onready var red_sprite_half = $RedSpriteHalf
 onready var green_sprite_full = $GrenSpriteFull
@@ -13,6 +20,23 @@ onready var green_sprite_half = $GreenSpriteHalf
 func _ready():
 	match_dimension(WorldStats.DIMENSION)
 	WorldStats.connect("dimension_shift", self, "match_dimension")
+
+func _physics_process(delta):
+	
+	accelerate_towards_point(wander_controller.target_position, delta)
+	wander_controller.set_position(wander_controller.start_position)
+	
+	if wander_controller.can_see_unit():
+		wander_controller.start_timer(3)
+		wander_controller.target_position = global_position
+		velocity = Vector2.ZERO
+
+	velocity = move_and_slide(velocity)
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, FRICTION * delta)
+	flip_sprite()
 
 func match_dimension(state):
 	red_sprite_full.visible = false
@@ -35,6 +59,13 @@ func match_dimension(state):
 		hurt_box.monitorable = true
 		hurt_box.monitoring = true
 
+func flip_sprite():
+	if velocity.x != 0:
+		red_sprite_full.flip_h = velocity.x > 0
+		red_sprite_half.flip_h = velocity.x > 0
+		green_sprite_full.flip_h = velocity.x > 0
+		green_sprite_half.flip_h = velocity.x > 0
+	
 func _on_HurtBox_invincible_start():
 	blink_anim_player.play("start")
 
---
+++ Utils/Controllers/DetectionWanderController.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+signal arrived
+
+onready var detection_zones = $DetectionZones
+onready var timer = $Timer
+onready var start_position = global_position
+onready var target_position = Vector2.ZERO
+
+var target_node: Node = null
+
+func _ready():
+	update_target_position()
+	randomize()
+		
+func update_target_position():
+	var available_nodes: Array = []
+	for detection_zone in detection_zones.get_children():
+		if detection_zone != target_node:
+			available_nodes.append(detection_zone)
+	var length = len(available_nodes)
+	var next_node_index = randi() % length
+	target_node = available_nodes[next_node_index]
+	target_position = target_node.global_position
+	var test = 0
+
+func can_see_unit():
+	for detection_zone in detection_zones.get_children():
+		if detection_zone.get_child(0).can_interact() and detection_zone == target_node:
+			emit_signal("arrived")
+			return true
+	return false
+
+func get_time_left():
+	return timer.time_left
+
+func start_timer(duration):
+	target_node = null
+	timer.start(duration)
+
+func set_position(position):
+	global_position = position
+
+func _on_Timer_timeout():
+	update_target_position()
