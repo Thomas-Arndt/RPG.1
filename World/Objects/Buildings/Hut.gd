29c22a3b25fa424aeb5ab948ee212280d4553a4b
Thomas Arndt <arndtt42@gmail.com>
2023-01-26 21:32:30 -0800

    Fixed disabling collision shape for hut

--- World/Objects/Buildings/Hut.gd
+++ World/Objects/Buildings/Hut.gd
@@ -23,23 +23,23 @@ func match_dimension(state):
 		red_sprite.visible = true
 		green_sprite.visible = false
 		shadow_sprite.visible = true
-		collision_shape.disabled = false
+		collision_shape.set_deferred("disabled", false)
 		emit_signal("set_Scene_Link_Active", true)
 	elif state == true and show_red == false:
 		red_sprite.visible = false
 		green_sprite.visible = false
 		shadow_sprite.visible = false
-		collision_shape.disabled = true
+		collision_shape.set_deferred("disabled", true)
 		emit_signal("set_Scene_Link_Active", false)
 	elif state == false and show_green == true:
 		red_sprite.visible = false
 		green_sprite.visible = true
 		shadow_sprite.visible = true
-		collision_shape.disabled = false
+		collision_shape.set_deferred("disabled", false)
 		emit_signal("set_Scene_Link_Active", true)
 	elif state == false and show_green == false:
 		red_sprite.visible = false
 		green_sprite.visible = false
 		shadow_sprite.visible = false
-		collision_shape.disabled = true
+		collision_shape.set_deferred("disabled", true)
 		emit_signal("set_Scene_Link_Active", false)
