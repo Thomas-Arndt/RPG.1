361079e9e72e953a6cef21f7f1a3a98c5103e428
Thomas Arndt <arndtt42@gmail.com>
2023-02-23 15:52:07 -0800

    First steps towards 'In-Between' dimensions areas

--- Utils/Stats/WorldStats.gd
+++ Utils/Stats/WorldStats.gd
@@ -4,7 +4,8 @@ signal dimension_shift(state)
 
 export(bool) var DIMENSION = false
 
-var player_spawn_vector: Vector2 = Vector2(270, 165)
+#var player_spawn_vector: Vector2 = Vector2(270, 165)
+var player_spawn_vector: Vector2 = Vector2(0, 0)
 var player_spawn_direction: Vector2 = Vector2.DOWN
 
 var save_block : String = "0"
