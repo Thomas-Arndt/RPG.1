347c15c61f180c66f99dbfd1d58c05ad28e68bc6
Thomas Arndt <arndtt42@gmail.com>
2025-02-08 09:59:11 -0700

    RPG-44 - Resolved issue with packaging choreograpy for cutscenes

--- Utils/Actions/CutsceneTriggerAction.gd
+++ Utils/Actions/CutsceneTriggerAction.gd
@@ -1,21 +1,22 @@
-extends Node2D
+extends Node
+class_name CutSceneTriggerAction
 
-export var active : bool = false
-
-var action = 0
-var triggered = false
-var player = null
+signal finished
 
 onready var actor_controllers = $ActorControllers
-onready var trigger_area = $TriggerArea
+
+export var active: bool
+
+var triggered: bool = false
+var player: Node = null
+var action: int = 0
 
 func _ready():
-	trigger_area.connect("body_entered", self, "_on_TriggerArea_body_entered")
 	for actor in actor_controllers.get_children():
 		actor.connect("finished", self, "_on_Actor_finished")
 		actor.connect("release_player", self, "release_player")
 
-func _on_TriggerArea_body_entered(body):
+func interact() -> void:
 	if active and not triggered:
 		start_cutscene(get_tree().get_nodes_in_group("Player")[0])
 
@@ -29,24 +30,11 @@ func start_cutscene(node):
 			actor.package_choreography()
 			actor.run_cut_scene()
 
-func _on_Actor_finished():
-	if len(actor_controllers.get_children()) > action:
-		action += 1
-	
-func release_player():
-	player.paused(false)
-	
-func reset():
-	action = 0
-	active = true
-	triggered = false
-
 func save():
 	var save_dict = {
 		"filename" : get_filename(),
 		"path" : get_path(),
+		"class" : "CompletedQuestAction",
 		"active" : active,
-		"triggered": triggered
 	}
 	return save_dict
-
